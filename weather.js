29b4084b42d9c52ed3d8f26776ca6456201dc402
Анатолий Григорьев <grigorevanatoliy93@gmail.com>
2025-11-27 12:22:18 +0700

    наброски скелета CLI

--- weather.js
+++ weather.js
@@ -1,8 +1,18 @@
 #!/usr/bin/env node
-import {} from "./helpers/args.js";
+import { getArgs } from "./helpers/args.js";
 const initCLI = () => {
   const args = getArgs(process.argv);
   console.log(args);
+  if (args.h) {
+    //Вывод хелпа -h это help
+  }
+  if (args.s) {
+    //Сохранение города -s это sity
+  }
+  if (args.t) {
+    //Сохранение токена -t это токен
+  }
+  //вывести погоду
 };
 
 initCLI();
