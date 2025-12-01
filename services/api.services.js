ce53b62e2bb60f1d761ef7e41266c2250d641f64
Анатолий Григорьев <grigorevanatoliy93@gmail.com>
2025-12-01 14:42:43 +0700

    исправлен путь до API (https) и путь до сервиса (storage.service.js)

--- services/api.services.js
+++ services/api.services.js
@@ -1,12 +1,12 @@
 import https from "https";
-import { getKeyValue, TOKEN_DICTIONARY } from "./storage.service";
+import { getKeyValue, TOKEN_DICTIONARY } from "./storage.service.js";
 
 const getWether = async (city) => {
   const token = await getKeyValue(TOKEN_DICTIONARY.token);
   if (!token) {
     throw new Error("Не задан ключ API, -t [API_KEY]");
   }
-  const url = new URL("http://api.openweathermap.org/data/2.5/weather");
+  const url = new URL("https://api.openweathermap.org/data/2.5/weather");
   url.searchParams.append("q", city);
   url.searchParams.append("appid", token);
   url.searchParams.append("lang", "ru");
